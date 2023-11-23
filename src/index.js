440990eee42e329df1ebb89dcde98b4a84c49d5d
Tran-Anh-Duc <trananhducty@gmail.com>
2023-11-24 00:36:59 +0700

    more_nodejs

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path'); // Import thư viện path để làm việc với đường dẫn
+const { log } = require('console');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+// Cấu hình handlebars làm template engine
+app.engine('hbs',handlebars({
+    extname : '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  return res.render('home');
+});
+
+
+app.get('/news', (req, res) => {
+  return res.render('news'); 
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
